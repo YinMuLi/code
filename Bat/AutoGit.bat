3e792a3cf3ca54f7301a67726d030e184498d041
YinMu <1621780679@qq.com>
2023-12-03 11:19:51 +0800

    2023/12/03 周日 11:19 AUTO

--- Bat/AutoGit.bat
+++ Bat/AutoGit.bat
@@ -3,10 +3,11 @@ setlocal enabledelayedexpansion
 @REM 推送地址数组,修改此数组的同时需要修改下面FOR循环的
 set repos[0]=C:\Users\Administrator\Documents\My" "Games\Terraria\tModLoader\ModSources\Branch
 set repos[1]=D:\Work\School
+set repos[2]=D:\Work\Code
 @REM 推送的信息
 set message=%date% %time:~0,5% AUTO
 @REM 遍历数组
-FOR /L %%i IN (0, 1, 1) DO (
+FOR /L %%i IN (0, 1, 2) DO (
     @REM 切换到指定目录
     cd !repos[%%i]!
     @REM 设置状态变量为空
